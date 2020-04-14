57567b7f00495da2eedb70f8b707ab970ab8565c
samjustin <sam8231@163.com>
2020-04-14 17:43:49 +0800

    feat: 设计模式

---
+++ src/main/java/com/example/designpattern/behavior/strategy/discount/Order.java
@@ -0,0 +1,16 @@
+package com.example.designpattern.behavior.strategy.discount;
+
+/**
+ * 订单类
+ *
+ * @author sunyajun
+ * @date 2020/4/14 11:56 AM
+ */
+public class Order {
+
+    private OrderType orderType;
+
+    public OrderType getOrderType() {
+        return orderType;
+    }
+}
