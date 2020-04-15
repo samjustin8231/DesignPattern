848e6e6681d7109a93d75133a9e99d55e8fa43d9
samjustin <sam8231@163.com>
2020-04-15 11:24:34 +0800

    feat: 添加工厂模式; 代理模式实例; 组合模式

---
+++ src/main/java/com/example/designpattern/cteator/factory/simplefactory/PhoneFactory.java
@@ -0,0 +1,17 @@
+package com.example.designpattern.cteator.factory.simplefactory;
+
+/**
+ * @author sunyajun
+ * @date 2020/4/15 11:17 AM
+ */
+public class PhoneFactory {
+
+    public Phone makePhone(String phoneType) {
+        if (phoneType.equalsIgnoreCase("MiPhone")) {
+            return new MiPhone();
+        } else if (phoneType.equalsIgnoreCase("iPhone")) {
+            return new IPhone();
+        }
+        return null;
+    }
+}
